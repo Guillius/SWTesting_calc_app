2fee05209c575dd065954d239dd5ea2bc4934759
Guillius <guillaume.de.ly@hotmail.com>
2016-04-28 14:39:36 +0200

    part 1 of Parser implementation todo: 	createVectorFromString() method 	finish parseInputString() method

--- src/swtesting_calc_app/Parser.java
+++ src/swtesting_calc_app/Parser.java
@@ -1,6 +1,12 @@
 
 package swtesting_calc_app;
 
+import com.sun.xml.internal.fastinfoset.tools.StAX2SAXReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Guillaume
@@ -13,20 +19,67 @@ public class Parser {
     private CLInterface cli;
 
     public Parser(CLInterface mainProgram) {
-        
         this.cli = mainProgram;
         this.errorMessage = "";
         this.lastAnswer = 0.0;
-        
     }
     
     /**
      * @todo implement parsing the inputstring => regex/split...
      * @return returns 0 if successful, -1 in case of an error
      */
-    public int parseInputString(String calcEntry){
-        //NOT YET IMPLEMENTED
-        return -1;
+    public int parseInputString(String input){
+        
+        int status = -1;
+        
+        Calculation calculationObj;
+        
+        String parts[] = input.split( " " );
+        
+        /* Filters out empty Strings from parts[]
+        in case the input has two spaces next to each other,
+        which would lead to an empty String in the parts[] array.
+        */
+        List<String> list = new ArrayList<>();
+        Collections.addAll( list, parts );
+        list.removeAll( Arrays.asList( "" ) );
+        parts = list.toArray( new String[list.size()] );
+        
+        if( parts.length < 2 ){
+            errorMessage = "Too little elements to parse as a calculation.";
+            return -1;
+        } else if( parts.length == 2 ){
+            calculationObj = new OneOperandCalculation( parts[0], Double.parseDouble( parts[1] ) );
+            status = 0;
+        } else if( parts.length == 3 ){
+            if( parts[0].contains( "v" ) || parts[2].contains( "v" ) ){
+                if( parts[0].contains("v") && parts[2].contains( "v" ) ){
+                    calculationObj = new VectorVectorCalculation( parts[1], createVectorFromString( parts[0] ), createVectorFromString( parts[2] ) );
+                    status = 0;
+                } else{
+                    if( parts[0].contains( "v" ) ){
+                        calculationObj = new VectorScalarCalculation( parts[1], createVectorFromString( parts[0] ), Double.parseDouble( parts[2] ) );
+                        status = 0;
+                    } else {
+                        calculationObj = new VectorScalarCalculation( parts[1], createVectorFromString( parts[2] ), Double.parseDouble( parts[0] ) );
+                        status = 0;
+                    }
+                }
+            } else if( parts[0].contains( "V" ) || parts[2].contains( "V" ) ){
+                errorMessage = "Vectors are defined with a small 'v'!";
+                return -1;
+            } else {
+                calculationObj = new TwoOperandCalculation( parts[1], Double.parseDouble( parts[0] ), Double.parseDouble( parts[2] ) );
+                status = 0;
+            }
+        } else {
+            errorMessage = "Too many elements to parse as a calculation.";
+            return -1;
+        }
+        
+        //execute the calculation object and deal with 0 of -1 as return value
+            /* @todo implement this */
+        return status;
         
     }
 
@@ -50,12 +103,13 @@ public class Parser {
     public String getErrorMessage() {
         return errorMessage;
     }
-
-    /**
-     * @param errorMessage the errorMessage to set
-     */
-    public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
+    
+    private double[] createVectorFromString( String vectorString ){
+        /* @todo implement this
+            NOT  YET IMPLEMENTED
+            @todo still test the implementation for this method!
+        */
+        return null;
     }
-
+    
 }//end of class
